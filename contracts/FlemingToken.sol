cc067d6fe8ca8c32587745865e3166dbf25bdfd6
Thomas Moran <thomas@optimizme.com>
2022-05-01 16:27:46 +0100

    token contract update

--- contracts/FlemingToken.sol
+++ contracts/FlemingToken.sol
@@ -14,38 +14,4 @@ contract FlemingToken is ERC20PresetMinterPauser {
     function burn(uint256 value) public onlyRole(BURNER_ROLE) override {
         super._burn(msg.sender, value);
     }
-
-/*
-    uint256 public unitsOneEthCanBuy  = 10;
-    address public tokenOwner;         // the owner of the token
-
-    // constructor will only be invoked during contract deployment
-    constructor(string memory name, string memory symbol)
-    ERC20(name, symbol) {
-        tokenOwner = msg.sender;       // address of the token owner
-        uint256 n = 1000;
-        // mint the tokens
-        _mint(msg.sender, n * 10**uint(decimals()));
-    }
-    // this function is called when someone sends ether to the token contract
-    receive() external payable {
-        // msg.value (in Wei) is the ether sent to the token contract
-        // msg.sender is the account that sends the ether to the token contract
-        // amount is the token bought by the sender
-        uint256 amount = msg.value * unitsOneEthCanBuy;
-
-        // ensure you have enough tokens to sell
-        require(balanceOf(tokenOwner) >= amount,
-            "Not enough tokens");
-
-        // transfer the token to the buyer
-        _transfer(tokenOwner, msg.sender, amount);
-
-        // emit an event to inform of the transfer
-        emit Transfer(tokenOwner, msg.sender, amount);
-
-        // send the ether earned to the token owner
-        payable(tokenOwner).transfer(msg.value);
-    }
-*/
 }
